867de1effce2cde6637e5bfded5c6bcf4c3f7ad9
joshuale <phuonglevu3112@gmail.com>
2023-01-10 23:45:24 +0800

    relocated modelling config functions to another file

--- src/model/cnn_model.py
+++ src/model/cnn_model.py
@@ -1,42 +1,39 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchsummary import summary
 import time
-from config.data_config import INPUT_WIDTH, INPUT_HEIGHT, INPUT_CHANNEL
 import pandas as pd
 import matplotlib.pyplot as plt
-import torch.optim as optim
 import cv2
 
 
-def default_loss():
-    return nn.CrossEntropyLoss()
+# def default_loss():
+#     return nn.CrossEntropyLoss()
 
 
-def default_optimizer(model, learning_rate = 0.001):
-    return optim.Adam(model.parameters(), lr = learning_rate)
+# def default_optimizer(model, learning_rate = 0.001):
+#     return optim.Adam(model.parameters(), lr = learning_rate)
 
 
-def get_default_device():
-    """Picking GPU if available or else CPU"""
-    if torch.cuda.is_available():
-        return torch.device('cuda')
-    else:
-        return torch.device('cpu')
+# def get_default_device():
+#     """Picking GPU if available or else CPU"""
+#     if torch.cuda.is_available():
+#         return torch.device('cuda')
+#     else:
+#         return torch.device('cpu')
 
 
-def model_prep_and_summary(model, device):
-    """
-    Move model to GPU and print model summary
-    """
-    # Define the model and move it to GPU:
-    model = model
-    model = model.to(device)
-    print('Current device: ' + str(device))
-    print('Is Model on CUDA: ' + str(next(model.parameters()).is_cuda))
-    # Display model summary:
-    summary(model, (INPUT_CHANNEL, INPUT_WIDTH, INPUT_HEIGHT))
+# def model_prep_and_summary(model, device):
+#     """
+#     Move model to GPU and print model summary
+#     """
+#     # Define the model and move it to GPU:
+#     model = model
+#     model = model.to(device)
+#     print('Current device: ' + str(device))
+#     print('Is Model on CUDA: ' + str(next(model.parameters()).is_cuda))
+#     # Display model summary:
+#     summary(model, (INPUT_CHANNEL, INPUT_WIDTH, INPUT_HEIGHT))
 
 
 class MyCnnModel(nn.Module):
